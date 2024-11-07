8e056b8a0a77cb7e0a800e0cb57361ff89b6ad15
Pythonforbasics <114282587+Themaster1112@users.noreply.github.com>
2024-11-07 18:40:46 +0000

    Pending changes exported from your codespace

---
+++ #This is your first project yay.py
@@ -0,0 +1,6 @@
+#This is your first project yay
+#This is my first project i ever did :D
+#Follow along :D
+Passphrase=input("Welcome to the bank of america vault please input the Manager password")
+if Passphrase == "90987" or "8765431":
+    print("Welcome dear MR.Bon")
